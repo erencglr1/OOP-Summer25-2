45c8f3b95f0e3154e83d2e50a3f6240a87715e62
erencglr1 <erencglr1980@icloud.com>
2025-03-13 11:13:50 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -4,3 +4,11 @@ idnex_number = "35095"
 nationality = "Turkish"
 starting_date = "03/13/2025"
 courses = ["Computer science"] 
+
+
+print(f"First Name: {first_name}")
+print(f"Last Name: {last_name}")
+print(f"Index Number: {index_number}")
+print(f"Nationality: {nationality}")
+print(f"Starting Date: {starting_date}")
+print(f"Courses: {', '.join(courses)}")
