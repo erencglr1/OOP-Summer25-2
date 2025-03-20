7b92c636ceb6f5e677f19629267edf45ee9f37ae
erencglr1 <erencglr1980@icloud.com>
2025-03-20 10:32:59 +0100

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,8 @@
+animals = [
+    {"name": "Doggy", "group": "Mammals", "number_of_legs": 4, "skills": ["Running", "Swimming", "Barking"]},
+    {"name": "Parrot", "group": "Birds", "number_of_legs": 2, "skills": ["Flying", "Singing"]},
+    {"name": "Elephant", "group": "Mammals", "number_of_legs": 4, "skills": ["Trunk usage", "Swimming", "Heavy lifting"]},
+    {"name": "Cow", "group": "Mammals", "number_of_legs": 4, "skills": ["Grazing", "Milk production"]},
+    {"name": "Snake", "group": "Reptiles", "number_of_legs": 0, "skills": ["Slithering", "Hunting"]}
+]
+print(animals)
